bfd5b6917e4a96a56ddb866cb4c472966565e1bb
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2026-04-21 18:58:02 +0200

    Add profile update form and country selection component

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,7 +1,11 @@
+import SelectCountry from "@/app/_components/SelectCountry";
+import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
+
 export const metadata = {
   title: "Update profile",
 };
 export default function Page() {
+  const nationality = "portugal";
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
@@ -12,6 +16,14 @@ export default function Page() {
         Providing the following information will make your check-in process
         faster and smoother. See you soon!
       </p>
+      <UpdateProfileForm>
+        <SelectCountry
+          name="nationality"
+          id="nationality"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          defaultCountry={nationality}
+        />
+      </UpdateProfileForm>
     </div>
   );
 }
