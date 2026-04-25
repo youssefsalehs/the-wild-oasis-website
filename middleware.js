e772ae7fe38aa0f152fc1848931c9fab145653df
youssefsalehs <117232407+youssefsalehs@users.noreply.github.com>
2026-04-25 23:38:43 +0300

    Update middleware config to match subpaths

--- middleware.js
+++ middleware.js
@@ -3,5 +3,6 @@ import { NextResponse } from "next/server";
 import { auth } from "./app/_lib/auth";
 export const middleware = auth;
 export const config = {
-  matcher: ["/account"],
+  matcher: ["/account/:path*"],
+  runtime: "nodejs",
 };
